eb066a7402389a97808f5d298d3a54ba23bdcfcb
ChrisBAshton <chrisashtonweb@gmail.com>
2017-04-02 13:50:46 +0100

    added PYTHONPATH and NODE_PATH env variables

--- paths/all.sh
+++ paths/all.sh
@@ -22,3 +22,7 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 export PATH=$PATH:$HOME/Library/Python/2.7/bin
+
+# these were in my ~/.bashrc, guess they need to be exposed too!
+export PYTHONPATH="/Library/Python/2.7/site-packages"
+export NODE_PATH="/usr/local/share/npm/lib/node_modules"
