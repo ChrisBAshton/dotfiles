5aa515048cb5ebf86cb9d1a694430b416e4b04db
ChrisBAshton <chrisashtonweb@gmail.com>
2017-04-02 13:48:49 +0100

    exported python path for pip install to work

--- paths/all.sh
+++ paths/all.sh
@@ -20,3 +20,5 @@ export PATH=/usr/local/bin:$PATH
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+export PATH=$PATH:$HOME/Library/Python/2.7/bin
