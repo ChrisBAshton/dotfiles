51a3d4c1c4471c601ea64fc0273785da67c0b02b
ChrisBAshton <chrisashtonweb@gmail.com>
2018-04-11 16:57:11 +0100

    stop using legacy version

--- scripts/bbc.sh
+++ scripts/bbc.sh
@@ -1,2 +1,2 @@
 # Load BBC Utils only if it exists
-BBC_UTILS=~/Sites/bbc/news/special/utils/legacy/bash_profile && test -f $BBC_UTILS && source $BBC_UTILS
+BBC_UTILS=~/Sites/bbc/news/special/utils/bash_profile && test -f $BBC_UTILS && source $BBC_UTILS
