ec6e5c450e4ea5bfcf3c997c79485a34fdef4efe
ChrisBAshton <chrisashtonweb@gmail.com>
2019-03-01 08:03:16 +0000

    removes security flaw
    see https://stackoverflow.com/questions/9679932/how-to-use-package-installed-locally-in-node-modules#comment33532258_9683472

--- paths/all.sh
+++ paths/all.sh
@@ -1,24 +1,14 @@
-export PATH=$PATH:./node_modules/.bin
-export PATH=$PATH:./bin/
-
 # to install Git via Homebrew
 export PATH=/usr/local/bin:$PATH
 
-export PATH=$PATH:/usr/local/bin:/usr/local/bin:$HOME/.rvm/gems/ruby-2.1.3/bin:$HOME/.rvm/gems/ruby-2.1.3@global/bin:$HOME/.rvm/rubies/ruby-2.1.3/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/texbin:$HOME/.rvm/bin:$HOME/.rvm/bin:$HOME/Documents/bbc/chromedriver:$HOME/bin
-
-export PATH=$HOME/.rvm/gems/ruby-2.0.0-p353/bin:$PATH
+# php
 export PATH=/usr/local/php5/bin:$PATH;
 
+# rvm
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
-
-export PATH=/usr/local/bin:$PATH
-
-
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
+# python
 export PATH=$PATH:$HOME/Library/Python/2.7/bin
 
 # these were in my ~/.bashrc, guess they need to be exposed too!
