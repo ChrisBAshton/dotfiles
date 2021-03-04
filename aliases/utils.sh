902f350b5ad5029c163e114d5cc3d4895adf819d
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-03-04 11:24:48 +0000

    Add curlr alias

--- aliases/utils.sh
+++ aliases/utils.sh
@@ -2,3 +2,5 @@ alias lsa="ls -la"
 alias get='find . -name'
 alias server='echo "open http://localhost:8000 now!"; python -m SimpleHTTPServer 8000 .'
 alias wine='wine64'
+# get response headers via curl - harder than it should be! https://catonmat.net/cookbooks/curl/print-response-headers
+alias curlr='curl -s -o /dev/null -D -' # e.g. curlr https://www.gov.uk
