df5c11c9867c6b891729d671c578fd8519b263ff
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-01-25 14:50:03 +0000

    Remove legacy Python paths
    I'm now using [asdf](https://asdf-vm.com/) to manage my runtimes.
    These lines were causing issues with my local Python setup
    (https://github.com/danhper/asdf-python).

--- paths/all.sh
+++ paths/all.sh
@@ -8,15 +8,10 @@ export PATH=/usr/local/php5/bin:$PATH;
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
-# python
-export PATH=$PATH:$HOME/Library/Python/2.7/bin
-export PATH=$PATH:$HOME/Library/Python/3.8/bin
-
 # NVM/NPM/Node
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # these were in my ~/.bashrc, guess they need to be exposed too!
-export PYTHONPATH="/Library/Python/2.7/site-packages"
 export NODE_PATH="/usr/local/share/npm/lib/node_modules"
