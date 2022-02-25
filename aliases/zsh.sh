baa89ccfe354105b952dcec55e8a03a3e688fec5
ChrisBAshton <chrisashtonweb@gmail.com>
2022-02-25 16:07:27 +0000

    Remove unused theme reload functionality
    I'm pretty happy just using one theme.

--- aliases/zsh.sh
+++ aliases/zsh.sh
@@ -1,30 +1,4 @@
-reload() {
-  # override the theme if we want, e.g. `reload -t 'agnoster'`
-  ZSH_THEME=""
-  while getopts ":t:" opt; do
-    case $opt in
-      t)
-        echo "Setting theme to '$OPTARG'"
-        ZSH_THEME="$OPTARG"
-        ;;
-    esac
-  done
-
-  if [ -z "$ZSH_THEME" ]; then
-    echo "Choosing theme at random..."
-    ZSH_THEME=random
-  fi
-  source ~/.zshrc
-}
-
-#if [ -z "$ZSH_THEME" ]; then
-#  echo "Welcome to your terminal. Choosing a theme at random..."
-#  echo "(Try a different one by running 'reload', or specify by running e.g. 'reload -t fino'."
-#  echo "These are all great themes: 'fino', 'agnoster', 'af-magic', 'steeef'"
-#  ZSH_THEME=random
-#fi
-
-ZSH_THEME=fino
+ZSH_THEME=fino # `agnoster`, `af-magic`, and `steeef` are good alternatives
 
 # Which plugins would you like to load?
 # Standard plugins can be found in ~/.oh-my-zsh/plugins/*
@@ -36,8 +10,3 @@ plugins=(git gitfast zsh-autosuggestions)
 source $ZSH/oh-my-zsh.sh
 
 setopt NO_NOMATCH
-
-# This must come AFTER the `oh-my-zsh.sh` sourcing
-if [ "$ZSH_THEME" = "random" ]; then
-  echo "Setting theme to $RANDOM_THEME"
-fi
