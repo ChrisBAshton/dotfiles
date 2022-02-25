dcaf5b67a5609866483ad4c966935a2713179c98
ChrisBAshton <chrisashtonweb@gmail.com>
2022-02-25 16:33:42 +0000

    Don't break if personal.sh is missing

--- zshrc.sh
+++ zshrc.sh
@@ -4,7 +4,7 @@ source ~/.dotfiles/globals.sh
 #### ALIASES ####
 #################
 source ~/.dotfiles/aliases/git.sh
-source ~/.dotfiles/aliases/personal.sh
+FILE=~/.dotfiles/aliases/personal.sh && test -f $FILE && source $FILE
 source ~/.dotfiles/aliases/utils.sh
 source ~/.dotfiles/aliases/work.sh
 
