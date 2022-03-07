89bf6a7e4b8aa200ad87a6c4988a40f48b58220d
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-03-07 16:38:25 +0000

    Remove rbenv references - I use asdf now
    See https://asdf-vm.com/

--- globals.sh
+++ globals.sh
@@ -1,18 +1,10 @@
 export ZSH="$HOME/.dotfiles/.oh-my-zsh"
 
-# force load rbenv to ensure correct version of ruby used
-export PATH="$PATH:~/.rbenv/shims"
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
 # to install Git via Homebrew
 export PATH=/usr/local/bin:$PATH
 
 # php
 export PATH=/usr/local/php5/bin:$PATH;
-
-# rvm
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 # NVM/NPM/Node
 export NVM_DIR="$HOME/.nvm"
