7d853d51669ce80e975c274f11cadd9fd1b790bb
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-05-13 11:25:11 +0100

    Add `context` function for easier switching of GOV.UK EKS contexts

--- aliases/work.sh
+++ aliases/work.sh
@@ -1,6 +1,14 @@
 alias k="kubectl"
 alias run="govuk-docker-run bundle exec"
 alias gbundle="govuk-docker-run bundle"
+# govuk kubernetes
+context() {
+    # Usage: `context integration poweruser`
+    # Arg 1: integration, staging, production
+    # Arg 2: readonly, poweruser, administrator
+    echo "Switching GOV.UK kubernetes context to $1 (${2:-poweruser})..."
+    k config use-context $1 && eval $(gds aws govuk-$1-${2:-poweruser} -e --art 8h)
+}
 
 # govuk-docker
 export PATH=$PATH:~/govuk/govuk-docker/exe
