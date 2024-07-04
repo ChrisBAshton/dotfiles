47247e1fc36da29499f5075602b35e75390af6b7
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-07-04 11:11:08 +0100

    Add kconsole, krake and awsconsole commands
    Also renames context to kcontext for consistency.

--- aliases/work.sh
+++ aliases/work.sh
@@ -1,13 +1,39 @@
 alias k="kubectl"
 alias run="govuk-docker-run bundle exec"
 alias gbundle="govuk-docker-run bundle"
-# govuk kubernetes
-context() {
-    # Usage: `context integration poweruser`
+
+awsconsole() {
+    # Usage: `awsconsole integration poweruser`
     # Arg 1: integration, staging, production
     # Arg 2: readonly, poweruser, administrator
+    # Defaults to poweruser role.
+    echo "Opening AWS console on $1 (${2:-poweruser})..."
+    gds aws govuk-$1-${2:-poweruser} -l
+}
+
+# govuk kubernetes
+kcontext() {
+    # Usage: `kcontext integration poweruser`
+    # Arg 1: integration, staging, production
+    # Arg 2: readonly, poweruser, administrator
+    # Defaults to poweruser role.
     echo "Switching GOV.UK kubernetes context to $1 (${2:-poweruser})..."
     k config use-context $1 && eval $(gds aws govuk-$1-${2:-poweruser} -e --art 8h)
+}
+
+kconsole() {
+    # Usage `kconsole manuals-publisher`
+    # Arg 1: app to connect to
+    echo "Opening a rails console on $1 on $(eval k config current-context)..."
+    k exec -it deploy/$1 -- rails c
+}
+
+krake() {
+    # Usage `krake asset-manager assets:redirect[content_id,url]`
+    # Arg 1: app to connect to
+    # Arg 2: rake task to run
+    echo "Running rake task on $1 on $(eval k config current-context): $2"
+    k exec -it deploy/$1 -- rake $2
 }
 
 # govuk-docker
